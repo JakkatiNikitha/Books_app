5c07b3ff555e20359b877f8ccc96d70d2d1d8fe3
JakkatiNikitha <135969743+JakkatiNikitha@users.noreply.github.com>
2023-10-03 20:09:04 +0530

    Add files via upload

---
+++ sqlserver_config.py
@@ -0,0 +1,10 @@
+dbConfig = 	{ 
+ 	'driver' : 'SQL Server',
+	'Server' : 'DESKTOP-BSH688M\SQLSERVER2019',
+	'Database' : "mybooks",
+	'username' : "DESKTOP-BSH688M/User",
+	'password' :  ''
+   		}
+#  import this module (nothing but filename) to access 
+# the details for the security reasons .
+
